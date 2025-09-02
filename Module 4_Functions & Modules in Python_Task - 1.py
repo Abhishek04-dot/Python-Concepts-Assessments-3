e589178b4697538b295c2937c25cc2fe71878c4a
Abhishek04-dot <guptaabhishek0403@gmail.com>
2025-09-02 15:52:04 +0530

    Module 4_Functions & Modules in Python_Task - 1.py

--- Module 4_Functions & Modules in Python_Task - 1.py
+++ Module 4_Functions & Modules in Python_Task - 1.py
@@ -1,8 +1,13 @@
-def factorial(num):
+def factorial(n):
     fact = 1
-    for i in range(1,num+1):
+    for i in range(1,n+1):
         fact *= i
     return fact
-num = int(input('Enter a number: '))
-results = factorial(num)
-print(results)
+n = int(input('Enter a number: '))
+results = factorial(n)
+if n<0:
+    print('Enter a whole number')
+elif n<=1:
+    print('The factorial of ' + str(n) + ' is: 1')
+else:
+    print('The factorial of ' + str(n) + ' is: ' + str(results))
